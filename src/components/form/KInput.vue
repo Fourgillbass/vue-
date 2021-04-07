94e5a5c98ca737e0a8e6c243e94f78bc185e8cf1
Fourgillbass <18705155673@163.com>
2021-04-07 23:50:54 +0800

    input组件封装

---
+++ src/components/form/KInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input :type="type" :value="value" @input="onInput" v-bind="$attrs">
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    oninput (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
